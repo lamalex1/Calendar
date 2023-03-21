23a4e571a428a7484fdd21d9718fb1ca03f4f02d
lamalex1 <hongnghia987@gmail.com>
2023-03-21 22:03:22 +0700

    Commit 2

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-console.log("loaded script")
-const currentDate = document.querySelector(".current-date"),
-daysTag = document.querySelector(".days"),
-prevNextIcon = document.querySelectorAll(".icon span");
+const currentDate = document.querySelector(".current-date");
+daysTag = document.querySelector(".days");
+prevNextIcon = document.querySelectorAll(".icons span");
 
 let date = new Date(),
 currYear = date.getFullYear(),
@@ -13,25 +12,25 @@ const months = ["January", "February", "March","April","May","June","July",
 const renderCalendar = () => {
     let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
     lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
-    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(),
+    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay();
     lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
     let liTag = "";
 
     for (let i = firstDayofMonth; i > 0 ;i --){
-        liTag += `<li class "inactive">${ lastDateofLastMonth - i + 1}</li>`;
+        liTag += `<li class "inactive">${lastDateofLastMonth - i + 1}</li>`;
     }
 
     for (let i = 1; i <= lastDateofMonth; i++) {
         let isToday = i === date.getDate() && currMonth === new Date().getMonth()
                         && currYear === new Date().getFullYear() ? "active" : "";
-        liTag += `<li>${i}</li>`;
+        liTag += `<li class="${isToday}">${i}</li>`;
     }
 
     for (let i = lastDayofMonth; i < 6; i++) {
-        liTag += `<li class "inactive">${i - lastDateofLastMonth + 1}</li>`;        
+        liTag += `<li class "inactive">${i - lastDayofMonth + 1}</li>`;        
     }
 
-    currentDate.innerText = `${[currMonth]} ${currYear}`;
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
     daysTag.innerHTML = liTag;
 }
 renderCalendar();
